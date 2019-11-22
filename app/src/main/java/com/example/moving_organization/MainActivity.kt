8a065738ba4e05dda2af04a04762af74238f2232
SKumar74 <sanjk7400@gmail.com>
2019-11-22 11:50:22 -0800

    Initial commit!

---
+++ app/src/main/java/com/example/moving_organization/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moving_organization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/moving_organization/Read.kt
@@ -0,0 +1,12 @@
+package com.example.moving_organization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class readActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read)
+    }
+}
